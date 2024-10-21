03ada7075852d602f90cee6044c08bd994985173
YeriAddict <denis.leang@gmail.com>
2024-10-20 20:39:29 -0400

    Added logic to save images following the same structure from input

--- IrisRecognition.py
+++ IrisRecognition.py
@@ -1,7 +1,12 @@
 import os
 import cv2
+import numpy as np
+from scipy.spatial import distance
 
-DATASET_FOLDER = "data"
+from IrisLocalization import IrisLocalizer
+
+INPUT_FOLDER = "input"
+OUTPUT_FOLDER = "output"
 
 def load_dataset(folder_path):
     data = {"training": [], "testing": []}
@@ -16,19 +21,34 @@ def load_dataset(folder_path):
                 img_path = os.path.join(training_folder, img_file)
                 image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                 if image is not None:
-                    data["training"].append(image)
+                    data["training"].append((image, img_path))
 
             for img_file in os.listdir(testing_folder):
                 img_path = os.path.join(testing_folder, img_file)
                 image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                 if image is not None:
-                    data["testing"].append(image)
+                    data["testing"].append((image, img_path))
+
     return data["training"], data["testing"]
 
+def save_localized_images(dataset, folder_path, output_root_folder):
+    for image, original_image_path in dataset:
+        # Recreate the original folder structure under the new root folder 'localized'
+        relative_path = os.path.relpath(original_image_path, folder_path)  # Get relative path to preserve structure
+        localized_image_path = os.path.join(output_root_folder, relative_path)
+
+        # Create the necessary directories in the output folder
+        localized_image_dir = os.path.dirname(localized_image_path)
+        os.makedirs(localized_image_dir, exist_ok=True)
+
+        # Process and save the image
+        iris_localizer = IrisLocalizer(image)
+        pupil_coordinates = iris_localizer.localize()
+        iris_localizer.save_image(localized_image_path)
+
 def main():
-    training, testing = load_dataset(DATASET_FOLDER)
-    print("Training images:", len(training))
-    print("Testing images:", len(testing))
+    training, testing = load_dataset(INPUT_FOLDER)
+    save_localized_images(training, INPUT_FOLDER, OUTPUT_FOLDER + "/localized")
 
 if __name__ == "__main__":
     main()
