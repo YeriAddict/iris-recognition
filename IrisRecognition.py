82547103e0aa09d53ea811e3ca1fdb43f04ec719
Denis Leang <95890184+YeriAddict@users.noreply.github.com>
2024-10-23 14:52:44 -0400

    Iris normalization (#4)
    * Added class to localize pupil and sclera circles
    
    * Modified logic to detect iris more accurately
    
    * Changed data file folder to "input"
    
    * Added folders to ignore in gitignore
    
    * Added logic to save images following the same structure from input
    
    * Fine tuned some values for detect iris function
    
    * Added logic to save localized images and pupil coordinates and added classes in main
    
    * Added original image path to localized images array
    
    * Wrote code to normalize the irises

--- IrisRecognition.py
+++ IrisRecognition.py
@@ -1,35 +1,121 @@
 import os
 import cv2
 import numpy as np
-from scipy.spatial import distance
 
 from IrisLocalization import IrisLocalizer
+from IrisNormalization import IrisNormalizer
 
+# Global constants
 INPUT_FOLDER = "input"
 OUTPUT_FOLDER = "output"
+LOCALIZED_FOLDER = "/localized"
+NORMALIZED_FOLDER = "/normalized"
 
-def load_dataset(folder_path):
-    data = {"training": [], "testing": []}
-    for eye_folder in os.listdir(folder_path):
-        eye_path = os.path.join(folder_path, eye_folder)
-        if os.path.isdir(eye_path):
-            subfolders = sorted(os.listdir(eye_path))
-            training_folder = os.path.join(eye_path, subfolders[0])
-            testing_folder = os.path.join(eye_path, subfolders[1])
-            
-            for img_file in os.listdir(training_folder):
-                img_path = os.path.join(training_folder, img_file)
-                image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                if image is not None:
-                    data["training"].append((image, img_path))
+class DataLoader:
+    """
+    Singleton class for loading training and testing data from a specified directory structure.
 
-            for img_file in os.listdir(testing_folder):
-                img_path = os.path.join(testing_folder, img_file)
-                image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                if image is not None:
-                    data["testing"].append((image, img_path))
+    Attributes
+    ----------
+        input_path (str): The path to the directory containing the data.
+        training (list): A list of tuples containing training images and their file paths.
+        testing (list): A list of tuples containing testing images and their file paths.
 
-    return data["training"], data["testing"]
+    Methods
+    -------
+        create() -> DataLoader:
+            Creates and returns a singleton instance of the DataLoader class.
+        load() -> tuple:
+            Loads the training and testing data from the specified directory structure.
+            Returns a tuple containing the training and testing data.
+    """
+    _instance = None
+
+    def __init__(self):
+        self.input_path = INPUT_FOLDER
+        self.training = None
+        self.testing = None
+
+    @classmethod
+    def create(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+            cls._instance.__init__()
+        return cls._instance
+
+    def load(self):
+        data = {"training": [], "testing": []}
+        for eye_folder in os.listdir(self.input_path):
+            eye_path = os.path.join(self.input_path, eye_folder)
+            if os.path.isdir(eye_path):
+                subfolders = sorted(os.listdir(eye_path))
+                training_folder = os.path.join(eye_path, subfolders[0])
+                testing_folder = os.path.join(eye_path, subfolders[1])
+                
+                for img_file in os.listdir(training_folder):
+                    img_path = os.path.join(training_folder, img_file)
+                    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                    if image is not None:
+                        data["training"].append((image, img_path))
+
+                for img_file in os.listdir(testing_folder):
+                    img_path = os.path.join(testing_folder, img_file)
+                    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                    if image is not None:
+                        data["testing"].append((image, img_path))
+
+        self.training = data["training"]
+        self.testing = data["testing"]
+
+        return self.training, self.testing
+
+class IrisRecognizer:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+        self.input_path = INPUT_FOLDER
+        self.output_path = OUTPUT_FOLDER
+        self.localized_images_path = LOCALIZED_FOLDER
+        self.normalized_images_path = NORMALIZED_FOLDER
+
+        # Iris Localization
+        self.localized_images = []
+        self.pupils_coordinates = []
+
+        # Iris Normalization
+        self.normalized_images = []
+
+    def localize_irises(self):
+        for image, original_image_path in self.dataset:
+            relative_path = os.path.relpath(original_image_path, self.input_path)
+            localized_image_path = os.path.join(self.output_path + self.localized_images_path, relative_path)
+            localized_image_directory = os.path.dirname(localized_image_path)
+            os.makedirs(localized_image_directory, exist_ok=True)
+
+            iris_localizer = IrisLocalizer(image)
+            localized_image, pupil_coordinates = iris_localizer.localize_iris()
+            iris_localizer.save_image(localized_image_path)
+
+            self.localized_images.append((localized_image, original_image_path))
+            self.pupils_coordinates.append(pupil_coordinates)
+        return self.localized_images, self.pupils_coordinates
+    
+    def normalize_irises(self):
+        for localized_image, pupil_coordinates in zip(self.localized_images, self.pupils_coordinates):
+            image = localized_image[0]
+            original_image_path = localized_image[1]
+
+            relative_path = os.path.relpath(original_image_path, self.input_path)
+            normalized_image_path = os.path.join(self.output_path + self.normalized_images_path, relative_path)
+            normalized_image_directory = os.path.dirname(normalized_image_path)
+            os.makedirs(normalized_image_directory, exist_ok=True)
+
+            iris_normalizer = IrisNormalizer(image, pupil_coordinates)
+            normalized_image = iris_normalizer.normalize_iris()
+            iris_normalizer.save_image(normalized_image_path)
+
+            self.normalized_images.append((normalized_image, original_image_path))
+        return self.normalized_images
 
 def save_localized_images(dataset, folder_path, output_root_folder):
     for image, original_image_path in dataset:
@@ -47,8 +133,10 @@ def save_localized_images(dataset, folder_path, output_root_folder):
         iris_localizer.save_image(localized_image_path)
 
 def main():
-    training, testing = load_dataset(INPUT_FOLDER)
-    save_localized_images(training, INPUT_FOLDER, OUTPUT_FOLDER + "/localized")
+    training, testing = DataLoader.create().load()
+    training_iris_recognizer = IrisRecognizer(training)
+    localized_images, pupil_coordinates = training_iris_recognizer.localize_irises()
+    normalized_images = training_iris_recognizer.normalize_irises()
 
 if __name__ == "__main__":
     main()
